9d55bc800d8d8d7e785ebe12196dd136fb933f09
David Sellén <dase1001@student.miun.se>
2016-04-01 11:00:11 +0200

    Cleaner repo

--- src/main/java/Log.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Dwe on 2016-02-28.
- */
-public class Log {
-}
--- src/main/resources/public/js/hello.js
+++ src/main/resources/public/js/hello.js
@@ -6,7 +6,22 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+map.zoomControl.setPosition('topright');
+
 setTimeout(function(){map.invalidateSize();}, 100);
+
+function selectWorksite(item){
+    var index = $scope.selected.indexOf(item.id);
+    if(index !== -1) {
+        $scope.selected.splice(index, 1);
+        $scope.selectedData['stems'] -= item.stems;
+        $scope.selectedData['worksites'] -= 1;
+    } else {
+        $scope.selected.push(item.id);
+        $scope.selectedData['stems'] += item.stems;
+        $scope.selectedData['worksites'] += 1;
+    }
+}
 
 app.controller('AppController', function($scope, $http) {
     $scope.totalStems = 0;
@@ -14,41 +29,38 @@ app.controller('AppController', function($scope, $http) {
     $http.get('/worksites/listAll').success(function(data) {
         $scope.worksites = data.map(JSON.parse);
 
+
         angular.forEach($scope.worksites, function(item){
-                $scope.totalStems = $scope.totalStems + item.stems
-                
-                L.circle([51.508, -0.11], 500, {
-                    color: 'red',
-                    fillColor: '#f03',
-                    fillOpacity: 0.5
-                }).addTo(mymap);
-
-                map.addCircles(item.longitude, item.latitude, popup=ct$type, weight = 3, radius=40,
-                                 color="#ffa500", stroke = TRUE, fillOpacity = 0.8)
-
+                $scope.totalStems = $scope.totalStems + item.stems;
+                var circle = L.circleMarker([item.latitude, item.longitude], {
+                    color: 'RED',
+                    fill: true,
+                    fillColor: 'RED',
+                    fillOpacity: 0.6,
+                    weight: 2,
+                    clickable: true
+                });
+                circle.setRadius(8);
+                circle.bindPopup("Worksite: " + item.id + " </br> Trees: " + item.stems);
+                circle.on('mouseover', function(e){
+                    marker.openPopup();
+                });
+                circle.on('click', function onClick(e) {
+                    alert("click");
+                    //selectWorksite(item);
+                });
+                circle.addTo(map);
         });
     });
 
     $scope.selectedData = {'stems' : 0, 'worksites' : 0 };
     $scope.selected = [];
-    $scope.select = function(item){
-        var index = $scope.selected.indexOf(item.id);
-        if(index !== -1) {
-          $scope.selected.splice(index, 1);
-          $scope.selectedData['stems'] -= item.stems;
-          $scope.selectedData['worksites'] -= 1;
-        } else {
-            $scope.selected.push(item.id);
-            $scope.selectedData['stems'] += item.stems;
-            $scope.selectedData['worksites'] += 1;
-        }
-    }
+    $scope.select = selectWorksite(item);
 })
 
 // Form for creating a dataset
 .controller('formController', function($scope, $http){
     $scope.submitQuery=function(){
-
         if(!$scope.field.group) return;
 
         /*alert($scope.field.group);
